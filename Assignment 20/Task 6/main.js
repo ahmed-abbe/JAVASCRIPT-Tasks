c707b362edac4e6b71ba6f99bc0e950041566b68
ahmed-abbe <113259428+ahmed-abbe@users.noreply.github.com>
2023-07-19 14:10:13 +0300

    Another solution

--- Assignment 20/Task 6/main.js
+++ Assignment 20/Task 6/main.js
@@ -41,6 +41,21 @@ if (chosen === 1) {
     ] = myFriends;
 }
 console.log(title, age, available ? "Available" : "Not Available", skill);
+
+// another solution
+/*
+    let chosen = 3;
+
+let myFriends = [
+    { title: "Osama", age: 39, available: true, skills: ["HTML", "CSS"] },
+    { title: "Ahmed", age: 25, available: false, skills: ["Python", "Django"] },
+    { title: "Sayed", age: 33, available: true, skills: ["PHP", "Laravel"] },
+];
+
+let {title: t, age: a, available: w, skills: [, skill]} = myFriends[chosen - 1]
+console.log(t, a, w ? "Available" : "Not Available", skill)
+*/
+
 // If chosen === 1
 /*
     ("Osama");
